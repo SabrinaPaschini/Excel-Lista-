ef9f3ff1847b8dcf051826c65607f8da6108abd9
Sabrina Paschini <sabrina.paschini@hotmail.com>
2023-06-12 10:38:28 -0300

    Primeiro commit Lista Excel +Python

---
+++ TESTE PLANILHA 4.py
@@ -0,0 +1,32 @@
+import openpyxl
+
+#CRIAR UMA PLANILHA
+
+book = openpyxl.Workbook()
+
+#COMO VISUALIZAR PAGINAS EXISTENTES
+
+print(book.sheetnames)
+
+#COMO CRIAR UMA PAGINA
+
+book.create_sheet('nomes')
+
+#COMO SELECIONAR UMA PÁGINA
+
+#SELECIONANDO uma GUIA
+
+nomes_page = book ['nomes']
+
+nomes_page.append(['VAZIA','Segunda','Terça','Quarta','Quinta','Sexta','Sábado','Domingo'])
+
+nomes_page.append(['Sabrina'])
+nomes_page.append(['Ana'])
+nomes_page.append(['Luana'])
+nomes_page.append(['Fernanda'])
+
+#SALVAR PLANILHA
+
+book.save('planilha de nomes.xlsx')
+
+
